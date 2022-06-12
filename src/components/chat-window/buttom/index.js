89ec6e10c25a64a371de73ede73667dfdb75e6c8
abhisheksharma <abhisheksharma131098@gmail.com>
2022-06-12 20:19:10 +0530

    created chat bottom, send messages to the chat

--- src/components/chat-window/buttom/index.js
+++ src/components/chat-window/buttom/index.js
@@ -1,8 +1,74 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react';
+import { InputGroup, Input, Icon, Alert } from 'rsuite';
+import firebase from 'firebase/app';
+import { useParams } from 'react-router';
+import {useProfile} from '../../../context/profile.context'
+import { database } from '../../../misc/firebase';
+
+function assembleMessage(profile, chatId){
+  return {
+    roomId: chatId,
+    author: {
+      name: profile.name,
+      uid: profile.uid,
+      CreatedAt: profile.createdAt,
+      ...(profile.avatar ? {avatar: profile.avatar} : {})
+    },
+    createdAt: firebase.database.ServerValue.TIMESTAMP
+  }
+};
 
 const ChatBottom = () => {
+
+  const [input, setInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const {chatId} = useParams();
+  const {profile} = useProfile();
+
+  const oninputChange = useCallback((value) => {
+    setInput(value);
+  },[]);
+
+  const onSendClick = async () => {
+    if(input.trim() === ''){
+      return;
+    }  
+    const msgData = assembleMessage(profile, chatId);
+    msgData.text = input;
+    const updates = {};
+    const messageId = database.ref('messages').push().key;
+    updates[`/messages/${messageId}`] = msgData;
+    updates[`/rooms/${chatId}/lastMessage`] = {
+      ...msgData,
+      msgId: messageId,
+    };
+    setIsLoading(true);
+    try {
+      await database.ref().update(updates);
+      setInput('');
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      Alert.error(error.message);
+    }
+  };
+
+  const onKeyDown = (ev) => {
+    if(ev.keyCode === 13){
+      ev.preventDefault();  
+      onSendClick();
+    }
+  };
+
   return (
-    <div>Buttom</div>
+    <div>
+      <InputGroup>
+        <Input placeholder='Write a message here...' value={input} onChange={oninputChange} onKeyDown={onKeyDown}/>
+        <InputGroup.Button color='blue' appearance='primary' onClick={onSendClick} disabled={isLoading}>
+          <Icon icon='send' />
+        </InputGroup.Button>
+      </InputGroup>
+    </div>
   );
 };
 
