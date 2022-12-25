a86f6698d873bf14b31612f0b6df08cdc27476ef
abhisheksharma <abhisheksharma131098@gmail.com>
2022-12-25 20:34:30 +0530

    display uploaded file

--- src/components/chat-window/buttom/index.js
+++ src/components/chat-window/buttom/index.js
@@ -4,6 +4,7 @@ import firebase from 'firebase/app';
 import { useParams } from 'react-router';
 import {useProfile} from '../../../context/profile.context'
 import { database } from '../../../misc/firebase';
+import AttachmentBtnModal from './AttachmentBtnModal';
 
 function assembleMessage(profile, chatId){
   return {
@@ -61,9 +62,41 @@ const ChatBottom = () => {
     }
   };
 
+  const afterUpload = useCallback(async (files)=>{
+    setIsLoading(true);
+
+    const updates = {};
+
+    files.forEach(file => {
+
+      const msgData = assembleMessage(profile, chatId);
+      msgData.file = file;
+
+      const messageId = database.ref('messages').push().key;
+      updates[`/messages/${messageId}`] = msgData;
+    })
+
+    const lastMsgId = Object.keys(updates).pop();
+
+    updates[`/rooms/${chatId}/lastMessage`] = {
+      ...updates[lastMsgId],
+      msgId: lastMsgId,
+    };
+
+    try {
+      await database.ref().update(updates);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      Alert.error(error.message);
+    }
+
+  },[chatId, profile]);
+
   return (
     <div>
       <InputGroup>
+        <AttachmentBtnModal afterUpload={afterUpload}/>
         <Input placeholder='Write a message here...' value={input} onChange={oninputChange} onKeyDown={onKeyDown}/>
         <InputGroup.Button color='blue' appearance='primary' onClick={onSendClick} disabled={isLoading}>
           <Icon icon='send' />
--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -8,10 +8,23 @@ import { useCurrentRoom } from '../../../context/current-room.context';
 import { auth } from '../../../misc/firebase';
 import IconBtnControl from './IconBtnControl';
 import { useHover, useMediaQuery } from '../../../misc/custom-hooks';
+import ImgBtnModal from './ImgBtnModal';
+
+const renderFileMessage = (file) => {
+
+    if(file.contentType.includes('image')){
+        return <div className='height-220'>
+            <ImgBtnModal src={file.url} fileName={file.name}/>
+        </div>
+    }
+
+    return <a href={file.url}>Download {file.name}</a>
+}
+
 
 const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
 
-    const {author, createdAt, text, likes, likeCount} = message;
+    const {author, createdAt, text, file, likes, likeCount} = message;
 
     const [selfRef, isHovered] = useHover();
     const isMobile = useMediaQuery(('(max-width: 992px)'));
@@ -61,7 +74,8 @@ const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
             </div>
 
             <div>
-                <span className='word-break-all'>{text}</span>
+                {text &&  <span className='word-break-all'>{text}</span>}
+                {file && renderFileMessage(file)}
             </div>
         </li>
     );
